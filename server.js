4525fc54a0689a59f149edf1f40b89e640f20b47
pvsgowtham <pvsg737@gmail.com>
2020-05-29 19:52:55 +0530

    this is 45th commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { execSync } = require("child_process");
 const express = require("express");
 const cors = require("cors");
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -10,49 +10,55 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.post("/commit", (req, res) => {
+app.post("/commit", async (req, res) => {
   console.log(req.body);
-  setTimeout(() => {
-    console.log("Got a new commit");
+  execSync(`git add .`);
+  var result = execSync(`git commit -m "this is 45th commit`);
+  console.log(result.toString());
+  var result = execSync(`git push origin master`);
 
-    var ans = "";
+  console.log(result.toString());
+  // setTimeout(() => {
+  //   console.log("Got a new commit");
 
-    var rand = Math.random();
-    rand = Math.round(rand * 1000 + 1);
-    var str = `This is ${rand} line`;
+  //   var ans = "";
 
-    setTimeout(() => {
-      fs.writeFileSync("data.txt", str, "utf8");
-    }, 1000);
+  //   var rand = Math.random();
+  //   rand = Math.round(rand * 1000 + 1);
+  //   var str = `This is ${rand} line`;
 
-    setTimeout(() => {
-      fs.readFile("data.txt", (e, data) => {
-        console.log(data.toString());
-        ans = ans + data.toString();
-      });
-    }, 2000);
+  //   setTimeout(() => {
+  //     fs.writeFileSync("data.txt", str, "utf8");
+  //   }, 1000);
 
-    setTimeout(() => {
-      execSync(`git add .`);
-    }, 3000);
+  //   setTimeout(() => {
+  //     fs.readFile("data.txt", (e, data) => {
+  //       console.log(data.toString());
+  //       ans = ans + data.toString();
+  //     });
+  //   }, 2000);
 
-    setTimeout(() => {
-      var result = execSync(`git commit -m "this is ${rand}th commit`);
-      console.log(result.toString());
-      ans = ans + result.toString();
-    }, 5000);
+  //   setTimeout(() => {
+  //     execSync(`git add .`);
+  //   }, 3000);
 
-    setTimeout(() => {
-      var result = execSync(`git push origin master`);
-      console.log(result.toString());
-      ans = ans + result.toString();
-    }, 8000);
+  //   setTimeout(() => {
+  //     var result = execSync(`git commit -m "this is ${rand}th commit`);
+  //     console.log(result.toString());
+  //     ans = ans + result.toString();
+  //   }, 5000);
 
-    setTimeout(() => {
-      res.send(ans);
-    }, 12000);
-  }, 20000);
-  res.send("complete");
+  //   setTimeout(() => {
+  //     var result = execSync(`git push origin master`);
+  //     console.log(result.toString());
+  //     ans = ans + result.toString();
+  //   }, 8000);
+
+  //   setTimeout(() => {
+  //     res.send(ans);
+  //   }, 12000);
+  // }, 20000);
+  // res.send("complete");
 });
 
 app.listen(8082, () => console.log("server listening on port 8082"));
